ee0b483972da1b74e879757107fccc67e194b694
Jakub Toth <jakubt4@gmail.com>
2015-02-28 02:26:35 +0100

    added test for Window

--- SynthesisOfTone/src/test/gui/WindowTest.java
+++ SynthesisOfTone/src/test/gui/WindowTest.java
@@ -1,5 +1,22 @@
 package test.gui;
 
+import gui.Window;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class WindowTest {
 
+    private Window window;
+
+    @Before
+    public void setUp() {
+        window = new Window();
+    }
+
+    @Test
+    public void WindowNullTest() {
+        Assert.assertNotNull(window);
+    }
 }

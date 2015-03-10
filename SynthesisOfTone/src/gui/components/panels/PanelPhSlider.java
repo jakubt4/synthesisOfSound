65c32b459d0f22aba549d56b77d528025b46f9db
Jakub Toth <jakubt4@gmail.com>
2015-03-10 23:49:26 +0100

    fix bug with use old labels in phases panel in AmplPhasesSetUpFrame for counting elements -> rewrite bounds of sliders phases panel
    Signed-off-by: Jakub Toth <jakubt4@gmail.com>

--- SynthesisOfTone/src/gui/components/panels/PanelPhSlider.java
+++ SynthesisOfTone/src/gui/components/panels/PanelPhSlider.java
@@ -8,7 +8,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.util.logging.Logger;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 
@@ -27,9 +26,6 @@ public class PanelPhSlider extends AbstractListenerSlider {
     @Override
     public void createComponentsForPanel() {
         for (int i = 0; i < NUMBER_OF_ELEMENTS.get(0); i++) {
-            JLabel label = new JLabel((i + 1) + ".");
-            this.add(label);
-
             JSlider slider = new JSlider();
             ComponentsUtil.setupSl(slider, 20, 200);
             addListener(slider, i);
@@ -64,7 +60,7 @@ public class PanelPhSlider extends AbstractListenerSlider {
 
     @Override
     public JPanel getPanel() {
-        ComponentsUtil.setPanelSettings(this, 365, 40, 235,
+        ComponentsUtil.setPanelSettings(this, 375, 40, 235,
                 (NUMBER_OF_ELEMENTS.get(0) * 32) + 28 + 40, false);
         return this;
     }
